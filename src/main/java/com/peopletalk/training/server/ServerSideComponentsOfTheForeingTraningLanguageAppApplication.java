62b32546968807f85dfb81aee57a2e25cba6b4d2
natalygussi <nataly.gussi@gmail.com>
2024-02-11 11:01:35 +0300

    init

---
+++ src/main/java/com/peopletalk/training/server/ServerSideComponentsOfTheForeingTraningLanguageAppApplication.java
@@ -0,0 +1,13 @@
+package com.peopletalk.training.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSideComponentsOfTheForeingTraningLanguageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSideComponentsOfTheForeingTraningLanguageAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peopletalk/training/server/ServerSideComponentsOfTheForeingTraningLanguageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peopletalk.training.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideComponentsOfTheForeingTraningLanguageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
